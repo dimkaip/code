ccfefcd3d9897fd03d3869f53aa8d51ca90de1c3
a <you@example.com>
2018-03-28 17:32:51 +0300

    Έκδοση 1.98

--- CalculatePlateTemperature-VERSION-2/main.c
+++ CalculatePlateTemperature-VERSION-2/main.c
@@ -11,6 +11,7 @@
 #define TEMPERATURE_RIGHT 2
 #define TEMPERATURE_UP 3
 #define TEMPERATURE_DOWN 4
+#define DIFF 10
 
 // MATRIXES
 
@@ -39,17 +40,26 @@ void init_m0()
     m0[ROWS-1][COLS-1]=(float)(TEMPERATURE_RIGHT+TEMPERATURE_DOWN)/2.0;
 }
 
-void print(float m[ROWS][COLS])
+void print_matrix_of_floats(float m[ROWS][COLS])
 {
     for(int i=0; i<ROWS; i++)
     {
         for(int j=0; j<COLS; j++)
-        {
             printf("%.2f ", m[i][j]);
-        }
         printf("\n");
     }
 }
+
+void print_matrix_of_integers(int m[ROWS][COLS])
+{
+    for(int i=0; i<ROWS; i++)
+    {
+        for(int j=0; j<COLS; j++)
+            printf("%i ", m[i][j]);
+        printf("\n");
+    }
+}
+
 
 //Αντιγράφει ένα μητρώο σε ένα άλλο.
 void copy(float from[ROWS][COLS],float to[ROWS][COLS])
@@ -80,14 +90,23 @@ void minmax(float m[ROWS][COLS], float*min, float* max)
         }
 }
 
+int return_class_of_int(float mij, int min, int max, float du)
+{
+    float f=mij-min;
+    int i=f/du;
+    if(i==10)i=9;
+    return i;
+}
+
 void normalize(float m[ROWS][COLS],int norm[ROWS][COLS], int onsize)
 {
     float min,max;
     minmax(m,&min,&max);
+    // διαφορική θερμοκρασία
     float du=(float)(max-min)/onsize;
     for(int i=1; i<ROWS; i++)
         for(int j=1; j<COLS; j++)
-            norm[i][j]=(m[i][j]-min)/du;
+            norm[i][j]=return_class_of_int(m[i][j],min,max,du);
 }
 
 int main()
@@ -95,7 +114,7 @@ int main()
 //αρχικοποίση της m0 με διόρθωση των γωνιακών.
     init_m0();
     printf("Εκτύπωση της m0\n");
-    print(m0);
+    print_matrix_of_floats(m0);
 
 //αντιγραφή στην m1
     copy(m0,m1);
@@ -104,20 +123,19 @@ int main()
     change(m1);
 
     printf("Εκτύπωση της m1\n");
-    print(m1);
+    print_matrix_of_floats(m1);
 
     printf("Εκτύπωση min και max του μητρώου m1\n");
 
     float min,max;
     minmax(m1,&min,&max);
 
-    printf("min is:%.2f max is:%.2f", min,max);
+    printf("min is:%.2f max is:%.2f \n", min,max);
 
-    // διαφορική θερμοκρασία
+    normalize(m1,m2,DIFF);
 
-    normalize(m1,m2,10);
     printf("Εκτύπωση normalize of μητρώου m1=m2\n");
-    print(m2);
+    print_matrix_of_integers(m2);
 
     return 0;
 }
